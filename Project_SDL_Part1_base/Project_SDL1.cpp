37a2c571dd5f3a8bb755f33df1d6355d43d15dea
camillemeuric <71633513+camillemeuric@users.noreply.github.com>
2022-12-15 14:42:58 +0100

    mouv loup

--- Project_SDL_Part1_base/Project_SDL1.cpp
+++ Project_SDL_Part1_base/Project_SDL1.cpp
@@ -63,8 +63,8 @@ animal::animal(const std::string& file_path, SDL_Surface* window_surface_ptr){
         fprintf(stderr, "Erreur SDL_LoadBMP : %s", SDL_GetError());
     }
 
-    pos.x = rand() % frame_width + 1; //position aleatoire de l'animal 
-    pos.y = rand() % frame_height + 1;
+    pos.x = rand() % frame_width - image_->w; //position aleatoire de l'animal 
+    pos.y = rand() % frame_height - image_->h;
 	pos.w = image_->w;
     pos.h = image_->h;
     
@@ -91,7 +91,7 @@ animal::~animal(){
 void sheep::move(){
 
     //bounces on the sides
-      if (pos.x == 1 || pos.x == frame_width -100) {
+      if (pos.x == 1 || pos.x == frame_width -image_->w) {
           switch (lastDir) {
           case 0:
               dir = UPRIGHT;    //this code checks if the sheep is right next to a wall 
@@ -114,7 +114,7 @@ void sheep::move(){
           }
       }
       //bounces on the top and bottom
-      if (pos.y == 1 || pos.y == frame_height -100) {
+      if (pos.y == 1 || pos.y == frame_height -image_->h) {
           switch (lastDir) {
           case 0:
               dir = DOWNLEFT;  //same thing down here but for
@@ -174,29 +174,112 @@ void sheep::move(){
  //----------------------------------------Partie Wolf ---------------------------------------
 void wolf::move(){
     //Selection d'une direction aléatoire entre -8 et 10 de base de faisait entre -1 et 1
-	int rand_dirX = 4* (rand()%10 - 8); // soit +1 soit -1
-	int rand_dirY = 4* (rand()%10- 8);
+/*	int rand_dirX = 2* (rand()%3 - 1); // soit +1 soit -1
+	int rand_dirY = 2* (rand()%3- 1);
 
-    std::cout << rand_dirX << "y : " << rand_dirY << "\n";
+    std::cout << "randx"<< rand_dirX << "randy : " << rand_dirY << "\n";
+    std::cout << "x:" << pos.x << "y : " << pos.y << "\n";
     
     // Verifie si loup ne touche pas le bord
-    if (pos.y == 1 || pos.y >= frame_height - 100) {
+    if (pos.y <= 1 || pos.y >= frame_height - image_->h) {
        // std::cout << "loup touche bord Y"  << "/n";
         if (rand_dirY > 0)
-            rand_dirY *=-1;
+            rand_dirY *=-rand_dirY;
     }
         //rand_dirY == 1 ? rand_dirY * (-1) : rand_dirY;
     
-    if (pos.x == 1 || pos.x >= frame_width - 100) 
+    if (pos.x <= 1 || pos.x >= frame_width - image_->w) 
     {
-       // std::cout << "loup touche bord X" <<"/n" ;
+       std::cout << "loup touche bord X" <<"/n" ;
         if (rand_dirX > 0)
-            rand_dirX *=-1;
+            rand_dirX *=-rand_dirX;
     }
         //rand_dirX == 1 ? rand_dirX * (-1) : rand_dirX;
      
     pos.x += rand_dirX;
     pos.y += rand_dirX;
+    */
+
+   if (pos.x == 1 || pos.x == frame_width -image_->w) {
+          switch (lastDir) {
+          case 0:
+              dir = DOWNRIGHT;  
+              break;           
+          case 1:             
+              dir = DOWNLEFT;
+              break;
+          case 2:
+              dir = UPLEFT;
+              break;
+          case 3:
+              dir = UPRIGHT;
+              break;
+          case 6: // pas mis de case 4 parceque on atteint pas les side si on va que en haut
+              dir = LEFT;//pas sur que ca soit atteint
+              break;
+          case 7: 
+              dir = RIGHT;
+              break;
+          }
+      }
+      //bounces on the top and bottom
+      if (pos.y == 1 || pos.y == frame_height -image_->h) {
+          switch (lastDir) {
+          case 0:
+              dir = DOWNRIGHT;  
+              break;           
+          case 1:             
+              dir = DOWNLEFT;
+              break;
+          case 2:
+              dir = UPLEFT;
+              break;
+          case 3:
+              dir = UPRIGHT;
+              break;
+          case 4: 
+              dir = DOWN; 
+              break;
+          case 5: 
+              dir = UP;
+              break; 
+          }
+      }
+    
+  // dir  = (rand()% 8) + 1;
+    //std::cout << "dir" << dir << "\n" ;
+      switch ((rand()% 8) + 1) {
+          case UPLEFT: 
+               pos.x--;     //this code moves the sheep 
+               pos.y--;     
+              break;       
+          case UPRIGHT:
+               pos.x++;
+               pos.y--;
+              break;
+          case DOWNLEFT:
+               pos.x--;
+               pos.y++;
+              break;
+          case DOWNRIGHT:
+               pos.x++;
+               pos.y++;
+              break;
+          case UP:
+               pos.x++;
+              break;
+          case DOWN:
+               pos.x--;
+              break;
+          case LEFT:
+               pos.y++;
+              break;
+          case RIGHT:
+               pos.y--;
+              break;
+      }
+      lastDir = dir; //it saves the last direction
+                  
 
 }
 
--- Project_SDL_Part1_base/Project_SDL1.h
+++ Project_SDL_Part1_base/Project_SDL1.h
@@ -14,7 +14,7 @@
 constexpr double frame_rate = 60.0;            // refresh rate
 constexpr double frame_time = 1. / frame_rate;
 constexpr unsigned frame_width = 1400;         // Width of window in pixel
-constexpr unsigned frame_height = 900;         // Height of window in pixel
+constexpr unsigned frame_height = 800;         // Height of window in pixel
 // Minimal distance of animal to the border of the screen
 constexpr unsigned frame_boundary = 100;
 
@@ -81,6 +81,9 @@ class sheep : public animal {
 //--------------------------------Partie Wolf --------------------------
 class wolf : public animal
 {
+	
+	private: 
+	int lastDir;
 	
     public:
 
